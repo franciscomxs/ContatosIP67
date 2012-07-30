3c3e4f772e0f47af54de2f8f1eb1cc7d0bdcfe70
ios2984 <ios2984@Mac-mini-de-caelum-2.local>
2012-07-30 14:27:09 -0300

    Initial Commit

---
+++ ContatosIP67/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios2984 on 30/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContatosIP67/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios2984 on 30/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
