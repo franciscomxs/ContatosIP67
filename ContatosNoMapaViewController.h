c3bea706688841088efa0ed06a470e60d988e972
ios2984 <ios2984@Mac-mini-de-calem.local>
2012-08-02 11:08:47 -0300

    Exercício 15.2

---
+++ ContatosNoMapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContatosNoMapaViewController.h
+//  ContatosIP67
+//
+//  Created by ios2984 on 02/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContatosNoMapaViewController : UIViewController
+
+@end
